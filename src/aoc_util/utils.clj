ef1bc2d06b5d28aca516f8fb78892307b091c032
steffen <es@nea-x.net>
2021-12-16 13:39:05 +0100

    Dijkstra

--- src/aoc_util/utils.clj
+++ src/aoc_util/utils.clj
@@ -1,13 +1,14 @@
 (ns aoc-util.utils
   "Helper functions"
-(:gen-class
+  (:gen-class
    :name aoc_util.utils
    :prefix "-"
    :main false
    :methods [#^{:static true} [numbersFromStr [String] java.util.List]
              #^{:static true} [strInt [String] Integer]])
   (:import [java.io BufferedReader StringReader])
-  (:require [clojure.edn :as edn]))
+  (:require [clojure.data.priority-map :refer [priority-map]]
+            [clojure.edn :as edn]))
 
 (defmacro save
   "Executes a body, if an excetption throws, 
@@ -20,7 +21,7 @@
          (println (.getMessage e#)))
        (.printStackTrace e#))))
 
- (defmacro define
+(defmacro define
   "Inspired from scheme to shortcut functions which return a
   lambda function
 
@@ -46,13 +47,13 @@
     :arglists '([^String n])}
   str->int
   (memoize
-    (fn [^String n]
-      (save
-        (Integer/parseInt n)))))
+   (fn [^String n]
+     (save
+      (Integer/parseInt n)))))
 
 (defn vec-remove
   "remove elem in coll"
-  [pos coll]
+  [coll pos]
   (into (subvec coll 0 pos) (subvec coll (inc pos))))
 
 (defn transpose [m]
@@ -67,7 +68,6 @@
   [^String str]
   (map edn/read-string (re-seq #"\d+" str)))
 
-
 (defn -numbersFromStr [^String s]
   (numbers-from-str s))
 
@@ -78,3 +78,19 @@
   ([^String input parser]
    (when (string? input)
      (mapv parser (line-seq (BufferedReader. (StringReader. input)))))))
+
+(defn dijkstra
+  "Computes single-source shortest path distances in a directed graph.
+
+  Given a node n, (f n), e.g. MAP, should return a map with the successors of n
+  as keys and their (non-negative) distance from n as vals.
+ 
+  Returns a map from nodes to their distance from start."
+  [start f]
+  (letfn [(remove-keys [m r]
+            (apply dissoc m (keys r)))]
+    (loop [q (priority-map start 0) r {}]
+      (if-let [[v d] (peek q)]
+        (let [dist (-> (f v) (remove-keys r) (update-vals #(+ d %)))]
+          (recur (merge-with min (pop q) dist) (assoc r v d)))
+        r))))
